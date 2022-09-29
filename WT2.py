69f6c47f3e30020258d45e20669b66b4075ea667
ChinmayDeore <114113228+ChinmayDeore@users.noreply.github.com>
2022-09-29 00:40:17 -0400

    Add files via upload

--- WT2.py
+++ WT2.py
@@ -1,6 +1,11 @@
+## Chinmay D. 9/21/2022
 def WorkTicketTwo():
+## Naming and starting the function
     print('''Twinkle, twinkle, little star, 
     \tHow I wonder what you are! \n\t\tUp above the world so high, 
     \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, 
-    \n\tHow I wonder what you are!''')
+    \tHow I wonder what you are!''')
+## Uses triple apostrophe to wrap text and also \n and 
+## \t to indent, space, and print the formatting appropriately 
 WorkTicketTwo()
+## Calls the function and ends the program
